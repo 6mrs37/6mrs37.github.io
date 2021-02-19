5b5b0cc059d9a8dcc3eae69ca96b57263f39cfe7
6mrs37 <69815667+6mrs37@users.noreply.github.com>
2021-02-19 14:34:36 -0500

    Create calendar-generator.js

---
+++ calendar-generator.js
@@ -0,0 +1,51 @@
+function dealWithColorChange(event) {
+    columnNum = event.data;
+    for (let monthNum = columnNum - 1; monthNum < 12; monthNum += columns) {
+        $("." + months[monthNum] + " td.normal").css("color", $("#color" + columnNum).val());
+        $("." + months[monthNum] + " td.faded").css("color", $("#color" + columnNum).val() + "50");
+        $("." + months[monthNum] + " th").css("background-color", $("#color" + columnNum).val());
+    }
+}
+
+function dealWithChangeInNumberOfColumns() {
+    columns = $("#numberOfColumns").val() * 1; // *1 converts to int
+    $(".color-selector").css("width", 100/columns + "%");
+    for (let columnNum = 1; columnNum < columns + 1; columnNum++) {
+        $("#color" + columnNum).show();
+        $("#label" + columnNum).show();
+        dealWithColorChange({data: columnNum});
+    }
+    for (let columnNum = columns + 1; columnNum < maxColumns + 1; columnNum++) {
+        $("#color" + columnNum).hide();
+        $("#label" + columnNum).hide();
+    }
+    $("table").css("width", 100 / columns + "%");
+    $("th").css("font-size", 15 / columns + "vw");
+    $("td").css("font-size", 9 / columns + "vw");
+    
+
+}
+
+let columns;
+const maxColumns = 6;
+const months = [
+	'jan', 
+	'feb', 
+	'mar', 
+	'apr', 
+	'may', 
+	'jun', 
+	'jul', 
+	'aug', 
+	'sep', 
+	'oct', 
+	'nov', 
+	'dec'
+];
+
+for (let columnNum = 1; columnNum < maxColumns + 1; columnNum++) {
+    $("#color" + columnNum).on("input", columnNum, dealWithColorChange);
+}
+
+dealWithChangeInNumberOfColumns();
+$("#numberOfColumns").on('input', dealWithChangeInNumberOfColumns);
